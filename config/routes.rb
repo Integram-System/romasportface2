606f981e0d7e7031e4ebc32269419a751142b339
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2016-02-16 07:46:55 +0100

    set index_magazine in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'mocks#index'
+
+  root 'blogs#index'
 
   resources :blogs
 
   get 'mocks/blog_small'
   get 'mocks/blog_single_small'
+  get 'mocks/index'
   get 'mocks/index_magazine'
 end
