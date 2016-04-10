41ef01b05cdb1d095889e0681e90776284e12012
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2016-04-10 17:49:01 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get 'mocks/blog_small'
   get 'mocks/blog_single_small'
+  get 'mocks/blog_single_small01'
   get 'mocks/index'
   get 'mocks/index_magazine'
 end
